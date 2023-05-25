4997bb94926288e1350b033e4d3a0308215f54c5
wingo-dev <rukshanlancer@gmail.com>
2023-05-25 14:51:14 +0800

    carousel addition

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,21 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import the carousel styles
+import { Carousel } from "react-responsive-carousel";
+import Image from "next/image";
+
 const HomePage = () => {
   return (
     <div className="container">
-      <h2>meetup website</h2>
+      <Carousel showThumbs={true}>
+        <div>
+          <Image src="/img/bg1.jpg" alt="Image 1" width={600} height={400} />
+        </div>
+        <div>
+          <Image src="/img/bg2.jpg" alt="Image 2" width={600} height={400} />
+        </div>
+        <div>
+          <Image src="/img/bg3.jpg" alt="Image 3" width={600} height={400} />
+        </div>
+      </Carousel>
     </div>
   );
 };
