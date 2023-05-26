1bcaf68eb7509b764e69af296c266bfcf83c7d5e
wingo-dev <rukshanlancer@gmail.com>
2023-05-26 17:26:41 +0800

    home page work

--- pages/index.js
+++ pages/index.js
@@ -5,15 +5,26 @@ import Image from "next/image";
 const HomePage = () => {
   return (
     <div className="container">
-      <Carousel showThumbs={true}>
+      <Carousel
+        showThumbs={true}
+        autoPlay={true}
+        interval={3000}
+        infiniteLoop={true}
+      >
         <div>
-          <Image src="/img/bg1.jpg" alt="Image 1" width={600} height={400} />
+          <Image src="/img/bg1.jpg" alt="Image 1" width={600} height={600} />
         </div>
         <div>
-          <Image src="/img/bg2.jpg" alt="Image 2" width={600} height={400} />
+          <Image src="/img/bg2.jpg" alt="Image 2" width={600} height={600} />
         </div>
         <div>
-          <Image src="/img/bg3.jpg" alt="Image 3" width={600} height={400} />
+          <Image src="/img/bg3.jpg" alt="Image 3" width={600} height={600} />
+        </div>
+        <div>
+          <Image src="/img/bg4.jpg" alt="Image 4" width={600} height={600} />
+        </div>
+        <div>
+          <Image src="/img/bg5.jpg" alt="Image 5" width={600} height={600} />
         </div>
       </Carousel>
     </div>
