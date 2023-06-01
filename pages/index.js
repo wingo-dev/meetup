09c376db9c5fd343680d136a78496a582464080f
wingo-dev <rukshanlancer@gmail.com>
2023-06-01 16:08:19 +0800

    meta tag add

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import the ca
 import { Carousel } from "react-responsive-carousel";
 import Image from "next/image";
 import MeetUp from "@/app/components/meetup";
+import Head from "next/head";
+import { Fragment } from "react";
 const cardData = [
   {
     title: "Meeting 1",
@@ -38,42 +40,48 @@ const cardData = [
 const HomePage = (props) => {
   // console.log(props.meetups);
   return (
-    <div className="container">
-      <Carousel
-        showThumbs={true}
-        autoPlay={true}
-        interval={3000}
-        infiniteLoop={true}
-      >
+    <Fragment>
+      <Head>
+        <title>meetups</title>
+        <meta name="description" content="Browse a huge meetups!" />
+      </Head>
+      <div className="container">
+        <Carousel
+          showThumbs={true}
+          autoPlay={true}
+          interval={3000}
+          infiniteLoop={true}
+        >
+          <div>
+            <Image src="/img/bg1.jpg" alt="Image 1" width={600} height={600} />
+          </div>
+          <div>
+            <Image src="/img/bg2.jpg" alt="Image 2" width={600} height={600} />
+          </div>
+          <div>
+            <Image src="/img/bg3.jpg" alt="Image 3" width={600} height={600} />
+          </div>
+          <div>
+            <Image src="/img/bg4.jpg" alt="Image 4" width={600} height={600} />
+          </div>
+          <div>
+            <Image src="/img/bg5.jpg" alt="Image 5" width={600} height={600} />
+          </div>
+        </Carousel>
         <div>
-          <Image src="/img/bg1.jpg" alt="Image 1" width={600} height={600} />
-        </div>
-        <div>
-          <Image src="/img/bg2.jpg" alt="Image 2" width={600} height={600} />
-        </div>
-        <div>
-          <Image src="/img/bg3.jpg" alt="Image 3" width={600} height={600} />
-        </div>
-        <div>
-          <Image src="/img/bg4.jpg" alt="Image 4" width={600} height={600} />
-        </div>
-        <div>
-          <Image src="/img/bg5.jpg" alt="Image 5" width={600} height={600} />
-        </div>
-      </Carousel>
-      <div>
-        <div className="card-container">
-          {props.meetups.map((meetup, index) => (
-            <MeetUp
-              imageUrl={meetup.imageUrl}
-              title={meetup.title}
-              content={meetup.content}
-              key={index}
-            />
-          ))}
+          <div className="card-container">
+            {props.meetups.map((meetup, index) => (
+              <MeetUp
+                imageUrl={meetup.imageUrl}
+                title={meetup.title}
+                content={meetup.content}
+                key={index}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
